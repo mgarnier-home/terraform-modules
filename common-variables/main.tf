d3fea80a9767d24ecd7afd9ac07ae3b4c36587da
Mathieu GARNIER <mgarnier11@gmail.com>
2024-08-05 12:52:13 +0000

    feat: added devcontainer base and common variables

---
+++ common-variables/main.tf
@@ -0,0 +1,34 @@
+# Terraform declarations
+terraform {
+  required_version = ">= 1.0"
+}
+
+output "common_extensions" {
+  value = [
+    "pkief.material-icon-theme",
+    "esbenp.prettier-vscode",
+    "github.copilot",
+    "naumovs.color-highlight",
+    "oderwat.indent-rainbow",
+    "ecmel.vscode-html-css",
+    "christian-kohler.npm-intellisense",
+    "christian-kohler.path-intellisense",
+    "mike-co.import-sorter",
+    "medo64.render-crlf",
+    "usernamehw.errorlens",
+    "formulahendry.auto-rename-tag",
+    "pflannery.vscode-versionlens",
+    "wix.vscode-import-cost",
+    "redhat.vscode-yaml",
+    "waderyan.gitblame",
+    "mhutchie.git-graph",
+    "donjayamanne.githistory",
+    "ms-azuretools.vscode-docker",
+    "formulahendry.docker-explorer",
+    "formulahendry.code-runner",
+    "GitHub.vscode-pull-request-github",
+    "aaron-bond.better-comments",
+    "pranaygp.vscode-css-peek",
+    "tomoki1207.pdf"
+  ]
+}
---
+++ devcontainer-base/scripts/install-dependencies.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+cat <<EOF > ~/.ssh/id_rsa
+-----BEGIN OPENSSH PRIVATE KEY-----
+$SSH_PRIVATE_KEY
+-----END OPENSSH PRIVATE KEY-----
+EOF
+chmod 600 ~/.ssh/id_rsa
+sudo service ssh start
+
+# Zsh Installation
+sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.2.0/zsh-in-docker.sh)"
+printf "ZSH Installed"
+
+# Task Installation
+if [ -z $INSTALL_TASK ]; then
+  sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b .local && \
+    echo "export PATH=\$PATH:~/.local" >> ~/.zshrc
+  printf "Task Installed"
+fi
+
+# Coder Installation
+if [ -z $INSTALL_CODER ]; then
+  curl -L https://coder.com/install.sh | sh
+  printf "Coder Installed"
+fi
+
+# Go Installation
+if [ -z $INSTALL_GO ]; then
+  curl -o go.tar.gz https://dl.google.com/go/go1.22.4.linux-amd64.tar.gz && \
+    sudo tar -C /usr/local -xzf go.tar.gz && \
+    rm go.tar.gz && \
+    echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.zshrc
+  printf "Go Installed"
+fi
+
+# NVM Installation
+if [ -z $INSTALL_NVM ]; then
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+  printf "NVM Installed"
+fi
+
+# Ansible Installation
+if [ -z $INSTALL_ANSIBLE ]; then
+  sudo add-apt-repository --yes --update ppa:ansible/ansible
+  DEBIAN_FRONTEND=noninteractive TZ=Europe/Paris apt-get install -y ansible
+  printf "Ansible Installed"
+fi
+
+sudo chsh -s /usr/bin/zsh $USER
+
+if [ -f /setup/setup-env.sh ]; then
+  /setup/setup-env.sh
+fi
+
