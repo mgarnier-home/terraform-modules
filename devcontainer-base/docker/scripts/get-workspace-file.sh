1d45c1c3d9e2eba935f2448401a0f08950113127
Mathieu GARNIER <mgarnier11@gmail.com>
2024-08-15 08:42:47 +0000

    feat: fixed get workspace file script, added p10k to zshrc in dockerfile

--- devcontainer-base/docker/scripts/get-workspace-file.sh
+++ devcontainer-base/docker/scripts/get-workspace-file.sh
@@ -1,23 +1,26 @@
-# List all folders in the /mnt/dev directory
-
-
 baseWorkspace=${1:-'{}'}
 
-folders="[]"
+# Get the folders from the base workspace file
+folders=$(jq '.folders // []' <<< "$baseWorkspace")
 
+# For each directory in /mnt/dev, add it to the workspace file
 for dir in /mnt/dev/*; do
   if [ -d $dir ]; then 
+    # Get the base name of the directory
     basename=$(basename $dir)
 
     echo "Adding $basename to workspace file"
 
+    # Add the directory to the folders array
     folders=$(jq --arg name "$basename" --arg path "$dir" '. + [{name: $name, path: $path}]' <<< "$folders")
   fi;
 done
 
+# Create a new JSON object with the folders array
 newJson=$(jq -n --argjson folders "$folders" '{folders: $folders}')
 
 echo "Merging with base workspace"
+# Merge the new JSON object with the base workspace file, that will fully replace the folders array, and keep the rest of the base workspace file
 newJson=$(echo "$baseWorkspace $newJson" | jq -s add)
 
 echo "New workspace file content:"
